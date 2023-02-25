76f028e81af4153ab8b476798def8e95a5f60c9f
jilenski <jtayhopon@gmail.com>
2023-02-25 14:01:34 +0800

    sync with live lesson

--- m2/src/Content.js
+++ m2/src/Content.js
@@ -1,18 +1,47 @@
 import './Content.css';
+import{ useState } from 'react';
 
 const Content = () =>{
+		const [nameTest, setName] = useState ('Wd42P 123');
+
+		const [count, setCount] = useState(0);
 
     const handleNameChanges = () => {
         const names = ['bob', 'juan', 'peter'];
         const int = Math.floor(Math.random() * 3);
-        return names[int];
+        // return names[int];
+				setName(names[int]);
       }
 
+		const handleClick = () => {
+			setCount(count + 1);
+			// console.log('You clicked it');
+		}
+
+		const handleClick2 = (name) => {
+			setCount(count + 1);
+			// console.log(name);
+		}
+
+		const handleClick3 = (e) => {
+			setCount(count + 1);
+			// console.log(e);
+			// console.log(e.target.innerText);
+		}
+
+		
     return(
         <main>
-            <p className='mysample'>
-                Hello {handleNameChanges()} !
+            <p onDoubleClick={handleClick} className='mysample'>
+            {nameTest}<br/>
+						{count}
             </p>
+
+						<button onClick={handleNameChanges}>Change Name</button><br/><br/>
+
+						<button onClick={handleClick}>Button One</button>
+						<button onClick={ () => handleClick2 ('bob')}>Button Two</button>
+						<button onClick={ (e) => handleClick3 (e)}>Button Three 123</button>
         </main>
     )
 }
